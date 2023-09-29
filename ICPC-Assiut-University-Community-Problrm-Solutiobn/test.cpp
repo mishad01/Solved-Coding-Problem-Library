035dc41fea9051b6fb4ddf77dc8a9fa21795c653
Mishad <mishadcs2002@gmail.com>
2023-09-29 23:19:30 +0600

    solved

---
+++ ICPC-Assiut-University-Community-Problrm-Solutiobn/test.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define n '\n'
+int main() {
+	int n; cin >> n;
+	int a[n];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+}
