c55030829868f8011ca10c85211ba21babde1403
Mishad <mishadcs2002@gmail.com>
2023-09-30 02:47:47 +0600

    vjudge

--- ICPC-Assiut-University-Community-Problrm-Solutiobn/test.cpp
+++ ICPC-Assiut-University-Community-Problrm-Solutiobn/test.cpp
@@ -3,14 +3,13 @@ using namespace std;
 #define ll long long
 #define nl '\n'
 int main() {
-    int N;cin>>N;
-
-    for (int i = 1; i <= N; ++i) {
-        for (int j = 1; j <= i; ++j) {
-            std::cout << "*";
-        }
-        std::cout << "\n";
-    }
-
-    return 0;
+	int a, b;
+	cin >> a >> b;
+	if (a == b)
+	{
+		cout << "Yes";	/* code */
+	}
+	else {
+		cout << "No";
+	}
 }
