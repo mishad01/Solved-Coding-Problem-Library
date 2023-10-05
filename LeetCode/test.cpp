4b9823b699127b7ac9a4cca4929e9eacf60a74cd
Mishad <mishadcs2002@gmail.com>
2023-10-06 02:06:30 +0600

    leetcode

--- LeetCode/test.cpp
+++ LeetCode/test.cpp
@@ -1,32 +1,22 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define nl '\n'
-int gcd(int a, int b) {
-	if (a == 0) return b;
-	if (b == 0) return a;
-	if (a < b) swap(a, b);
-	return gcd(a % b, b);
-}
-int findGCD(vector<int>& nums) {
-	int n = nums.size();
-	int minn = nums[0];
-	int maxx = 0;
-	int res = 0;
-	for (int i = 0; i < n; i++) {
-		minn=min(minn,nums[i]);
-		maxx = max(maxx, nums[i]);
-	}
-	cerr<<minn<<" "<<maxx;
-	res = gcd(minn, maxx);
-	return res;
-}
+#include <iostream>
+#include <set>
+
 int main() {
-	int n;
-	cin >> n;
-	vector<int>a(n);
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-	cout<<findGCD(a);
+    int arr[] = {5, 10, 3, 8, 15};
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+
+    std::set<int> se;
+
+    // Insert elements from the array into the set
+    for (int i = 0; i < arrSize; ++i) {
+        se.insert(arr[i]);
+    }
+
+    // Iterate through the set using an iterator
+    for (auto it = se.begin(); it != se.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
