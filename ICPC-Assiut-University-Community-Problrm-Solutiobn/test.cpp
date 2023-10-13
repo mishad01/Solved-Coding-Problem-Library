2d4fda39cc20df6618b901591693da8a059d266e
Mishad <mishadcs2002@gmail.com>
2023-10-13 10:49:18 +0600

    codeforce

--- ICPC-Assiut-University-Community-Problrm-Solutiobn/test.cpp
+++ ICPC-Assiut-University-Community-Problrm-Solutiobn/test.cpp
@@ -2,36 +2,14 @@
 using namespace std;
 #define ll long long
 #define nl '\n'
-int findLucky(vector<int>& arr) {
-	map<int, int>mp;
-	int num = 0;
-	int test =0;
-	for (int i = 0; i < arr.size(); i++) {
-		mp[arr[i]]++;
-	}
-	// for (int i = 0; i < arr.size(); i++) {
-	// 	cerr<<mp[arr[i]]<<endl;
-	// }
-	// for(auto x : mp){
-	// 	cerr<<x.first<<" "<<x.second<<endl;
-	// }
-	for (int i = 0; i < arr.size(); i++) {
-		if (mp[arr[i]] == arr[i]) {
-			num = max(num,arr[i]);
-		}
-	}
-	if(num>=1){
-		return num;
-	}
-	return -1;
-}
-int main() {
+int main(){
 	int n;
-	cin >> n;
-	vector<int>a{5,4,7,8,4,8,8,3,7,7,6,3,7,6,5,6,8,4,5,7,4,7,7,5,2,5,6,6,8,1,6,8,8,8,9,3,2,9};
-	// for (int i = 0; i < n; i++) {
-	// 	cin >> a[i];
-	// }
-	cout << findLucky(a);
-
+	cin>>n;
+	int fib[50];
+	 fib[1] = 0;
+	 fib[2] = 1;
+	for(int i = 3;i<=n;i++){
+		fib[i]=fib[i-1]+fib[i-2];
+	}
+	cout<<fib[n];
 }
--- ZPractice/test.cpp
+++ ZPractice/test.cpp
@@ -1,11 +1,41 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string>
+
 using namespace std;
-#define ll long long
-#define nl '\n'
+
+int count_operations(int n, int m, string x, string y) {
+    int ans = 0;
+
+    while (x != y) {
+        if (x.find(y) != string::npos) {
+            return ans + 1;
+        }
+
+        x += x;
+        ans++;
+
+        if (x.size() > n * m) {
+            return -1;
+        }
+    }
+
+    return ans;
+}
+
 int main() {
-	int a, b, c;
-	cin >> a >> b;
-	int x = a-b;
-	double discount_percentage = (double) x / a * 100;
-	cout <<fixed << setprecision(10)<< discount_percentage;
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+
+        string a, b;
+        cin >> a >> b;
+
+        int result = count_operations(n, m, a, b);
+        cout << result << endl;
+    }
+
+    return 0;
 }
