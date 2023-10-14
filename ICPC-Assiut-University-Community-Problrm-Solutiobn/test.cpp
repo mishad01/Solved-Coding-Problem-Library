80bc5bb9cab69bbf129994a34c0b990d7ac87f03
Mishad <mishadcs2002@gmail.com>
2023-10-14 14:10:31 +0600

    codeforce string

--- ICPC-Assiut-University-Community-Problrm-Solutiobn/test.cpp
+++ ICPC-Assiut-University-Community-Problrm-Solutiobn/test.cpp
@@ -1,16 +1,22 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define nl '\n'
 int main() {
-	int n;
-	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (i == 1 || i == n || j == 1 || j == n) {
-				cout << "* ";
-			} else {
-				cout << "  ";
-			}
-		}
-		cout << "\n";
+	int n; cin >> n;
+	int a[n];
+	int count = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
 	}
+	for (int i = 0; i < n; i++) {
+		if (a[i] == 0) {
+			count++;
+			swap(a[i],a[i+1]);
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		cout<<a[i]<<" ";
+	}
+	//cout<<count;
 }
--- Number Theory/test.cpp
+++ Number Theory/test.cpp
@@ -2,9 +2,19 @@
 using namespace std;
 #define ll long long
 #define nl '\n'
-const int N = 1e6;
-int d[N];
 int main(){
-    int x = 0/2;
-    cout<<x;
+    int q;
+    cin>>q;
+    int cs = 0;
+    while(q--){
+        int a;
+        cin>>a;
+        int div = 0;
+        for(int i = 1;1ll*i*i<=a;i++){
+            if(a%i==0){
+                div++;
+            }
+        }
+        cout<<"Case "<<++cs<<": "<<div<<nl;
+    }
 }
