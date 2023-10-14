5595851692edfd6a859d9be4048ad919b9ad1993
Mishad <mishadcs2002@gmail.com>
2023-10-15 00:51:49 +0600

    Number Theory

--- Number Theory/test.cpp
+++ Number Theory/test.cpp
@@ -2,44 +2,40 @@
 using namespace std;
 #define ll long long
 #define nl '\n'
-int main(){
-    int q;
+int const N = 1e6 + 9;
+int f[N];
+vector<int>primes;
+void seive() {
+    f[1] = true;
+    for (int i = 2; i < N; i++) {
+        f[i] = true;
+    }
+    for (int i = 2; i < N; i++) {
+        if (f[i]) {
+            for (int j = i + i; j < N; j += i) {
+                f[j] = false;
+            }
+        }
+    }
+    for (int i = 2; i < N; i++) {
+        if (f[i]) {
+            primes.push_back(i);
+        }
+    }
+
+}
+bool is 
+int main() {
+    seive();
+    ll q;
     cin>>q;
     while(q--){
-        ll n,a,b,c;
-        vector<int>divs;
-        cin>>n>>a>>b>>c;
-        int ans = 0;
-        for(int i = 1;i*i<=n;i++){
-            if(n%i==0){
-                divs.push_back(i);
-                if(i!=n/i){
-                    divs.push_back(n/i);
-                }
-            }
+        int n;
+        cin>>n;
+        int count = 0;
+        for(int i = 1;i<=n;i++){
+            
         }
-        for(auto x : divs){
-            if(x<=a){
-                for(auto y : divs){
-                    if(y<=b){
-                        // for(auto z : divs){
-                        //     if(z<=c){
-                        //         if(x*y*z==n){
-                        //             ++ans;
-                        //         }
-                        //     }
-                        // }
-                        if(n%(1ll*x*y)==0){
-                            int z = n/(x*y);
-                            if(z<=c){
-                                ++ans;
-                            }
-                        }
-                        
-                    }
-                }
-            }
-        }
-        cout<<ans<<nl;
+        cout<<count<<nl;
     }
 }
