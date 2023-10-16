c53b3e189b34e01cb736a1c506473fb55320893c
Mishad <mishadcs2002@gmail.com>
2023-10-16 23:45:17 +0600

    test

--- Number Theory/test.cpp
+++ Number Theory/test.cpp
@@ -2,23 +2,9 @@
 using namespace std;
 #define ll long long
 #define nl '\n'
+
 int main(){
-    double n , k,a ,operation;
-    cin >>n >> k>> a;
-    operation = (n * k) / a ;
-    // cout << operation ;
-    long long myNum = operation;
-
-    
-    if (operation == int(operation))
-    cout<< "int" ;
-    else if (operation == myNum) {
-        cout<< "long long" ;
-
-    }
-    else{
-        cout<< "double" ;
-
-    }
-    return 0;
+    int n;
+    cin>>n;
+    res(n);
 }
