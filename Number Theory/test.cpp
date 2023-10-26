e2bc1a18a63ee644f5f1998051fb1be538922c44
Mishad <mishadcs2002@gmail.com>
2023-10-26 20:57:40 +0600

    code

--- Number Theory/test.cpp
+++ Number Theory/test.cpp
@@ -3,8 +3,32 @@ using namespace std;
 #define ll long long
 #define nl '\n'
 
-int main(){
-    int n;
-    cin>>n;
-    res(n);
+int main() {
+    int q;
+    cin >> q;
+    map<char, int>mp;
+    int maxfreq = 0;
+    char maxchar;
+    if (q == 1) {
+        string x;
+        cin >> x;
+        cout << x;
+        //break;
+    }
+    else {
+        while (q--) {
+            string a;
+            cin >> a;
+            for (int i = 0; i < a.size(); i++)
+            {
+                mp[a[i]]++;
+                if (mp[a[i]] > maxfreq) {
+                    maxfreq = mp[a[i]];
+                    maxchar = a[i];
+                }
+            }
+        }
+        cout << maxchar;
+    }
+    //cout << maxchar;
 }
