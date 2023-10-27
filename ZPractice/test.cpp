1d59814d9b9db92a02be1d119128e9346795a43f
Mishad <mishadcs2002@gmail.com>
2023-10-27 11:41:43 +0600

    codeforce

--- ZPractice/test.cpp
+++ ZPractice/test.cpp
@@ -2,14 +2,45 @@
 using namespace std;
 #define ll long long
 #define nl '\n'
+bool isPrime(int n) {
+    int divisors = 0;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            divisors++;
+        }
+    }
+    if (divisors == 2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+bool isAlmostPrime(int n){
+    int primeDivisor = 0;
+    for(int i = 1;i<=n;i++){
+        if(n%i==0){
+            if(isPrime(i)){
+                primeDivisor++;
+            }
+        }
+    }
+    if(primeDivisor==2){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 int main() {
     int n;
-    cin>>n;
-    int count = 0;
-    while(n!=1){
-        int num = n%10;
-        count++;
-        n/=10;
+    cin >> n;int count = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        if(isAlmostPrime(i)==true){
+            count++;
+        }
     }
     cout<<count;
+
 }
