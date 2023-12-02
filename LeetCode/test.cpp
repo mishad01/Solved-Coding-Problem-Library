a076c0b46d817a3e4c4ed2e82de2549c1b6330f8
Mishad <mishadcs2002@gmail.com>
2023-12-02 10:33:31 +0600

    leetcode

--- LeetCode/test.cpp
+++ LeetCode/test.cpp
@@ -1,22 +1,10 @@
-#include <iostream>
-#include <set>
-
-int main() {
-    int arr[] = {5, 10, 3, 8, 15};
-    int arrSize = sizeof(arr) / sizeof(arr[0]);
-
-    std::set<int> se;
-
-    // Insert elements from the array into the set
-    for (int i = 0; i < arrSize; ++i) {
-        se.insert(arr[i]);
-    }
-
-    // Iterate through the set using an iterator
-    for (auto it = se.begin(); it != se.end(); ++it) {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
-
-    return 0;
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+int main(){
+    string a,b;
+    cin>>a>>b;
+    int t = a.find(b);
+    cout<<t;
 }
