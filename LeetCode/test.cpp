bfd7bbd0907e7a7cc72c6fdf608cd95cd572247f
mishad01 <mishadcs2002@gmail.com>
2024-03-13 23:10:15 +0600

    test

--- LeetCode/test.cpp
+++ LeetCode/test.cpp
@@ -5,24 +5,5 @@ using namespace std;
 int main() {
     string s1,s2;
     cin>>s1>>s2;
-    //int maxx = max(s1.size(),s2.size());
-    int count = 0;
-    set<char>s;
-    // for(int i = 0;i<s1.size();i++){
-    //    s.insert(s1[i]);
-    // }
-    // for(int i = 0;i<s2.size();i++){
-    //     s.insert(s2[i]);
-    // }
-    for(int i = 0;i<s1.size();i++){
-        for(int j = 0;j<s2.size();j++){
-            if(s1[i]==s2[j]){
-                count++;
-            }
-        }
-    }
-    // for(auto x : s){
-    //     count++;
-    // }
-    cout<<count;
+    cout<<s1<<s2;
 }
