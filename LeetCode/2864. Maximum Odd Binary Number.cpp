06e9f44be283ac54efc7af555c5b328eab6850fc
mishad01 <mishadcs2002@gmail.com>
2024-03-25 23:09:54 +0600

    2864. Maximum Odd Binary Number

---
+++ LeetCode/2864. Maximum Odd Binary Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int n = s.size();
+        sort(s.rbegin(),s.rend()); //sorting the string in descending order.
+    //For example, if you have a string s = "0101", after applying sort(s.rbegin(), s.rend()), the string becomes "1100".
+        for(int i = n-1;i>=0;i--){
+            if(s[i]=='1'){
+                swap(s[i],s[n-1]);
+            }
+        }
+        return s;
+    }
+};
