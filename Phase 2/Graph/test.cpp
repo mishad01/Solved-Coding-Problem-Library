04266fa538e2f0f6e142725e2d99adebbacfa1ca
mishad01 <mishadcs2002@gmail.com>
2024-07-10 22:39:00 +0600

    leetcode

--- Phase 2/Graph/test.cpp
+++ Phase 2/Graph/test.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int,int> store;
-        vector<int> ans;
-        for(int i=0;i<n;i++){
-            store[nums[i]]++;
+    int maximumUnits(vector<vector<int>>& bt, int ts) {
+        priority_queue<pair<int,int>>pq;
+        int count = 0;
+        for(auto x : bt){
+            pq.push({x[1],x[0]});
         }
-        for(auto& pair : store){
-            if(pair.second>n/3){
-                ans.push_back(pair.first);
+        int units = 0;
+        while(!pq.empty() && count<=ts){
+            if(count+pq.top().second<ts){
+                count +=pq.top().second;
+                units +=pq.top().first * pq.top().second;
+                pq.pop();
+            }else{
+                int quantity = ts-count;
+                count += quantity;
+                units += quantity * pq.top().first;  
+                pq.pop();
             }
         }
-        return ans;
+        return units;
     }
 };
