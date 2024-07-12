78411cf3496a4966af597b86a4762503829098e4
mishad01 <mishadcs2002@gmail.com>
2024-07-12 23:07:23 +0600

    171. Excel Sheet Column Number

--- Phase 2/Graph/test.cpp
+++ Phase 2/Graph/test.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
-    int maximumUnits(vector<vector<int>>& bt, int ts) {
-        priority_queue<pair<int,int>>pq;
-        int count = 0;
-        for(auto x : bt){
-            pq.push({x[1],x[0]});
+    int titleToNumber(string ct) {
+        int ans = 0;
+        for(auto x : ct){
+            int temp = x - 'A'+1;
+            ans = ans * 26 + temp;
         }
-        int units = 0;
-        while(!pq.empty() && count<=ts){
-            if(count+pq.top().second<ts){
-                count +=pq.top().second;
-                units +=pq.top().first * pq.top().second;
-                pq.pop();
-            }else{
-                int quantity = ts-count;
-                count += quantity;
-                units += quantity * pq.top().first;  
-                pq.pop();
-            }
-        }
-        return units;
+        return ans;
     }
 };
