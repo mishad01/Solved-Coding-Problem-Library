2acc07ad00afcb52340eb883ea631a5fe9e49759
mishad01 <mishadcs2002@gmail.com>
2024-07-13 23:42:35 +0600

    leetcode

--- Phase 2/Graph/test.cpp
+++ Phase 2/Graph/test.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    int titleToNumber(string ct) {
-        int ans = 0;
-        for(auto x : ct){
-            int temp = x - 'A'+1;
-            ans = ans * 26 + temp;
+    int sumOfMultiples(int n) {
+        int sum = 0;
+        for(int i = 1;i<=n;i++){
+            if(i%3==0 || i%5==0 ||i%7==0){
+                sum+=i;
+            }
         }
-        return ans;
+        return sum;
     }
 };
