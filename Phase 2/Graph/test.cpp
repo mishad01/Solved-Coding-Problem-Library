317fddda0fbb770e16a82bf37efd975b0fdd1aa6
mishad01 <mishadcs2002@gmail.com>
2024-07-14 23:42:10 +0600

    Leetcode

--- Phase 2/Graph/test.cpp
+++ Phase 2/Graph/test.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
-    int sumOfMultiples(int n) {
-        int sum = 0;
-        for(int i = 1;i<=n;i++){
-            if(i%3==0 || i%5==0 ||i%7==0){
-                sum+=i;
-            }
+static bool compare(const pair<int, string>& a, const pair<int, string>& b) {
+        return b.first < a.first; 
+    }
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        int n = names.size();
+        vector<pair<int,string>>hnp;
+
+        for(int i = 0;i<n;i++){
+            hnp.push_back({heights[i],names[i]});
         }
-        return sum;
+
+        sort(hnp.begin(),hnp.end(),compare);
+        vector<string>sorted_name;
+        for(auto x : hnp){
+            sorted_name.push_back(x.second);
+        }
+        return sorted_name;
     }
 };
