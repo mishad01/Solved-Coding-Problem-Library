83cde3919badedd374f7fced6ea8269d3f1395a2
mishad01 <mishadcs2002@gmail.com>
2024-07-26 21:47:17 +0600

    Binary Search Recursive

--- Phase 1 Remastered/Sort Your Life/binary_search_recursive.cpp
+++ Phase 1 Remastered/Sort Your Life/binary_search_recursive.cpp
@@ -2,33 +2,29 @@
 using namespace std;
 #define ll long long
 #define nl '\n'
-bool binary_search(int x,int size){
-    while(l<=r){
-        int mid = (l+r)/2;
-        cerr<<"Mid : "<<mid<<nl;
-        cerr<<"Left right : "<<l<<" "<<r<<nl;
-        if(x<a[mid]){
-            r = mid-1;
-        }else if(x>a[mid]){
-            l = mid+1;
-        }else if(x==a[mid]){
-            check = true;
-            break;
-        }
+int a[100];
+bool binary_search(int l,int r,int x){
+    if(l>r) return false;
+    int mid = (l+r)/2;
+    if(a[mid]==x) return true;
+    else if(x<a[mid]){
+        return binary_search(l,mid-1,x);
+    }else{
+        return binary_search(mid+1,r,x);
     }
 }
 int main() {
     int n,x;
     cin>>n>>x;
-    bool check = false;
-    int a[n];
     for(int i = 1;i<=n;i++){
     	cin>>a[i];
         //cout<<a[i]<<nl;
     }
     int l = 1,r = n;
-    if(check){
+    if(binary_search(l,r,x)){
         cout<<"YES";
+    }else{
+        cout<<"NO";
     }
 
 }
