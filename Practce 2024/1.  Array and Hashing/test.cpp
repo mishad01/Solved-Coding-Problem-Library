ade12ba44a6ad93d61611b537764ac3c8536f391
Mishad <mishadcs2002@gmail.com>
2024-09-21 00:41:44 +0600

    leetcode

--- Practce 2024/1.  Array and Hashing/test.cpp
+++ Practce 2024/1.  Array and Hashing/test.cpp
@@ -1,28 +1,12 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define nl '\n'
-int dominantIndex(vector<int>& nums) {
-    int maxv = 0;
-    for (auto x : nums) {
-        maxv = max(maxv, x);
-    }
-    auto it = find(nums.begin(), nums.end(), maxv);
-    int maxIndex = distance(nums.begin(), it);
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] * 2 > maxv && nums[i] != maxv) {
-            return -1;
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for (long long a = 0; a * a <= c; a++) {
+            long long b = sqrt(c - a * a);
+            if (a * a + b * b == c) {
+                return true;
+            }
         }
+        return false;
     }
-    return maxIndex;
-}
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int n; cin >> n;
-    vector<int>v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-    cout << dominantIndex(v);
-}
+};
