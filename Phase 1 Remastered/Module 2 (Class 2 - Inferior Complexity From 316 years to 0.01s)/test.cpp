89c478bac7aa3b97c63e516648c805581ac8cfc4
Mishad <mishadcs2002@gmail.com>
2024-10-27 22:46:29 +0600

    practice

--- Phase 1 Remastered/Module 2 (Class 2 - Inferior Complexity From 316 years to 0.01s)/test.cpp
+++ Phase 1 Remastered/Module 2 (Class 2 - Inferior Complexity From 316 years to 0.01s)/test.cpp
@@ -5,14 +5,26 @@ using namespace std;
 #define PI 3.1416
 #define ADD(a,b) (a+b) //We can also define function using macros
 #define square(x,y) x*y
-int32_main() { // notice that we are using int32_t instead of int here because int is redefined as long long
+
+bool check(char x){
+    string op = "+-*/%><|&";
+    return op.find(x)!=string ::npos;
+}
+int main() { // notice that we are using int32_t instead of int here because int is redefined as long long
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int a = 5 //Here int is Long long as we wrote int32_main
-    //cout<<PI;
-    int r = 3;
-    cout<<3.1416 * r<<nl;
-    cout<<ADD(5,5)<<nl;
-    cout<<square(5,5);
+    string s;
+    cin>>s;
+    string op = "";  // Proper initialization with double quotes
+    string temp = ""; 
+    for(auto x : s){
+        if (check(x)){
+            op+=x;
+        }else if((int)x >=48  && int(x)<=57){
+            temp+=x;
+        }
+    }
+    reverse(op.begin(),op.end());
+    cout<<op+temp<<" ";
 }
