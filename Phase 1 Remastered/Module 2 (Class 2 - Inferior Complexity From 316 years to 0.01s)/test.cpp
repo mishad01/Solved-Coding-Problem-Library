dab17468f7671bf4f88c24f81a23a310ef6f5f5e
Mishad <mishadcs2002@gmail.com>
2024-11-02 22:35:53 +0600

    cf

--- Phase 1 Remastered/Module 2 (Class 2 - Inferior Complexity From 316 years to 0.01s)/test.cpp
+++ Phase 1 Remastered/Module 2 (Class 2 - Inferior Complexity From 316 years to 0.01s)/test.cpp
@@ -2,29 +2,33 @@
 using namespace std;
 #define ll long long
 #define nl '\n'
-#define PI 3.1416
-#define ADD(a,b) (a+b) //We can also define function using macros
-#define square(x,y) x*y
 
-bool check(char x){
-    string op = "+-*/%><|&";
-    return op.find(x)!=string ::npos;
-}
-int main() { // notice that we are using int32_t instead of int here because int is redefined as long long
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    string s;
-    cin>>s;
-    string op = "";  // Proper initialization with double quotes
-    string temp = ""; 
-    for(auto x : s){
-        if (check(x)){
-            op+=x;
-        }else if((int)x >=48  && int(x)<=57){
-            temp+=x;
+    int t;
+    cin >> t;  // Number of test cases
+    while (t--) {
+        int n;
+        cin >> n;  // Number of notes in the melody
+        
+        vector<int> notes(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> notes[i];  // Read the notes
         }
+        
+        bool isPerfect = true;
+        for (int i = 1; i < n; ++i) {
+            int interval = abs(notes[i] - notes[i - 1]);
+            if (interval != 5 && interval != 7) {
+                isPerfect = false;
+                break;  // No need to check further if one interval is invalid
+            }
+        }
+        
+        cout << (isPerfect ? "YES" : "NO") << nl;
     }
-    reverse(op.begin(),op.end());
-    cout<<op+temp<<" ";
+
+    return 0;
 }
