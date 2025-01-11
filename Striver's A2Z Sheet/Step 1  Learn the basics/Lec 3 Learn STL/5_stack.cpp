8ac3d3a4033c673d87e37a0a334832c8b38e8571
Mishad <mishadcs2002@gmail.com>
2025-01-11 22:53:04 +0600

    stl practice

---
+++ Striver's A2Z Sheet/Step 1  Learn the basics/Lec 3 Learn STL/5_stack.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    stack<int>st;
+    //LIFO -> LAST IN FIRST OUT
+    st.push(1); //1
+    st.push(2); //2 1
+    st.push(3); //3 2 1
+    st.push(4); //4 3 2 1
+    st.emplace(5); //5 4 3 2 1
+    st.push(6);
+    cout<<st.top()<<nl;
+    cout<<st.size()<<nl;
+    return 0;
+}
