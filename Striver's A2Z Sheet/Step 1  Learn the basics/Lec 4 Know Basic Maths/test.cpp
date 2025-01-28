8e98b55b5ce76d5261e211f3bebcdbb70ebf19ca
Mishad <mishadcs2002@gmail.com>
2025-01-28 23:14:27 +0600

    practice

--- Striver's A2Z Sheet/Step 1  Learn the basics/Lec 4 Know Basic Maths/test.cpp
+++ Striver's A2Z Sheet/Step 1  Learn the basics/Lec 4 Know Basic Maths/test.cpp
@@ -5,13 +5,26 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int n;
-    cin>>n;
-    if(n<3){
-        cout<<"GOLD"<<nl;
-    }else if(n>=3 && n<6){
-        cout<<"SILVER"<<nl;
-    }else if(n>=6){
-        cout<<"BRONZE"<<nl;
+    string S, T, R;
+    cin >> S >> T>> R;
+   
+    unordered_map<char, int> pentagon = {
+        {'A', 0}, {'B', 1}, {'C', 2}, {'D', 3}, {'E', 4}
+    };
+
+    
+    int s1 = pentagon[S[0]], s2 = pentagon[S[1]];
+    int t1 = pentagon[T[0]], t2 = pentagon[T[1]];
+
+
+    int distS = min(abs(s1 - s2), 5 - abs(s1 - s2));
+    int distT = min(abs(t1 - t2), 5 - abs(t1 - t2));
+
+    if (distS == distT) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
     }
+
+    return 0;
 }
