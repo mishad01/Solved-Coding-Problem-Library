5ad6500cdb892115483b72c0d112bda6227f39a9
Mishad <mishadcs2002@gmail.com>
2025-02-24 11:18:52 +0600

    practice

--- Striver's A2Z Sheet/Step 2  Learn Important Sorting Techniques/Lec 1 Sorting-I/test.cpp
+++ Striver's A2Z Sheet/Step 2  Learn Important Sorting Techniques/Lec 1 Sorting-I/test.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int n;
-    cin>>n;
-    int a[4];
-    for(int i = 0;i<4;i++){
-        cin>>a[i];
+    int t;
+    cin>>t;
+    int n = t*2;
+    int [n];
+    for(int i = 0;i<n;i==){
+        
     }
-
 
     return 0;
 }
