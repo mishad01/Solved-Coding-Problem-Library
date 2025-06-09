d5f5d7b27b9290fdaa01e6a993f4eb029941b863
SAKIF RAHMAN MISHAD <109865291+mishad01@users.noreply.github.com>
2025-06-10 02:40:31 +0600

    sub array

--- Leetcode Blind 75/test.cpp
+++ Leetcode Blind 75/test.cpp
@@ -1,25 +1,20 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define ll long long
-#define nl '\n'
 
-int maxProfit(vector<int>& prices) {
-	int n = prices.size();
-	int maximum = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = i + 1; j < n; j++) {
-			int res = prices[j] - prices[i];;
-			if (prices[i] < prices[j]) {
-				maximum = max(maximum, (prices[j] - prices[i]));
-
-			}
-			cerr << res << nl;
-		}
-	}
-	return maximum;
-}
 int main() {
-	vector<int>v = {7, 1, 5, 3, 6, 4};
-	cout << maxProfit(v);
-
+  int n; cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = i; j < n; j++) {
+      // a[i..j] is a subarray of a
+      for (int k = i; k <= j; k++) {
+        cout << a[k] << ' ';
+      }
+      cout << '\n';
+    }
+  }
+  return 0;
 }
