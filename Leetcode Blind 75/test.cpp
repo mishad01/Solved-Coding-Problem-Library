f6e7cafa7dbadcba473d8f9ebf968337d18d9a3c
SAKIF RAHMAN MISHAD <109865291+mishad01@users.noreply.github.com>
2025-06-11 16:17:01 +0600

    73. Set Matrix Zeroes

--- Leetcode Blind 75/test.cpp
+++ Leetcode Blind 75/test.cpp
@@ -1,17 +1,27 @@
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        int maxSum = INT_MIN;
-        int currentSum = 0;
-
-        for (int i = 0; i < n; i++) {
-  	       int currSum = 0;
-           for (int j = i; j < n; j++) {
-              currSum +=nums[j];
-              maxSum = max(maxSum,currSum);
-        }
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    vector<vector<int>>matrix;
+    int n = 1;
+    for(int i = 0;i<3;i++){
+      vector<int>num;
+      for(int j = 0;j<4;j++){
+        num.push_back(n);
+        n++;
       }
-      return maxSum;
+      matrix.push_back(num);
     }
-};
+
+    for(int i = 0;i<3;i++){
+      for(int j = 0;j<matrix[0].size();j++){
+        cout<<matrix[i][j]<<" ";
+      }
+      cout<<endl;
+    }
+    cerr<<matrix.size();
+    return 0;
+}
--- Striver's A2Z Sheet/Step 1  Learn the basics/Lec 3 Learn STL/test.cpp
+++ Striver's A2Z Sheet/Step 1  Learn the basics/Lec 3 Learn STL/test.cpp
@@ -2,71 +2,10 @@
 using namespace std;
 #define ll long long
 #define nl '\n'
-void cyclicPattern(int n, int mod) {
-    cout << "2. Cyclic Pattern of Modulus Operations:" << endl;
-    for (int i = 1; i < n; ++i) {
-        cout << i << " % " << mod << " = " << (i % mod) << endl;
-    }
-}
-
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
-    
-    //Data Types
-    /*int a;
-    long long b;
-    float c;
-    double d;
-    char ch[10];*/
-
-    //Modules;
-
-    /*
-
-    Input:
-    5
-    33
-    25
-    75
-    88
-    56
-
-    Output:
-    3, 5, 5, 8, 6,6
-
-    */
-
-    // 1. Modulus Operations
-
-    /*int a = 17, b = 5;
-    cout << "1. Modulus Operations:" << endl;
-    cout << a << " % " << b << " = " << (a % b) << endl;*/
-
-
-
-
-
-
-
-
-    //scanf()
-    //cin >> ch;
-    //printf()
-    //cout<<ch;
-
-     //find smaller value 
-
-    int a = 100;
-    int b = 80;
-    int c = 50;
-    //Gretaer
-    if(a<b || a<c){
-        cout<<"A is smaller";
-    }else if(b<a || b<c){
-        cout<<"B is smaller";
-    }else{
-        cout<<"C is smaller";
-    }
+    vector<vector<int>>v;
+    for(int i = 0;)
+    return 0;
 }
