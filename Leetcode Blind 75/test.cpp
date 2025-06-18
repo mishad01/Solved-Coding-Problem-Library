1c3d59273bc8e854bfef2bc14f442b72c7456a8c
Mishad <mishadcs2002@gmail.com>
2025-06-19 00:27:16 +0600

    Full Integration of NetworkCaller with DI in Flutter

--- Leetcode Blind 75/test.cpp
+++ Leetcode Blind 75/test.cpp
@@ -5,23 +5,8 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    vector<vector<int>>matrix;
-    int n = 1;
-    for(int i = 0;i<3;i++){
-      vector<int>num;
-      for(int j = 0;j<4;j++){
-        num.push_back(n);
-        n++;
-      }
-      matrix.push_back(num);
-    }
-
-    for(int i = 0;i<3;i++){
-      for(int j = 0;j<matrix[0].size();j++){
-        cout<<matrix[i][j]<<" ";
-      }
-      cout<<endl;
-    }
-    cerr<<matrix.size();
+    int n,m;
+    cin>>n>>m;
+    int 
     return 0;
 }
--- Striver's A2Z Sheet/Step 2  Learn Important Sorting Techniques/Lec 1 Sorting-I/test.cpp
+++ Striver's A2Z Sheet/Step 2  Learn Important Sorting Techniques/Lec 1 Sorting-I/test.cpp
@@ -1,5 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    
+bool binarySearch(vector<int>&v, int target) {
+    int l = 0;
+    int r = v.size() - 1;
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if(v[mid]==target) return true;
+
+        if(v[mid]>target){
+            r = mid-1;
+        }else{
+            l = mid+1;
+        }
+    }
+    return false;
+}
+int main() {
+    vector<int> v = {1, 2, 3, 4, 5, 8, 9, 11};
+    int target = 11;
+    cout<<binarySearch(v,target);
+
 }
