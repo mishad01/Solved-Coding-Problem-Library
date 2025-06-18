846c7c5d761b40d5ba498e7d6fcc0cd23cb1e180
Mishad <mishadcs2002@gmail.com>
2025-06-18 23:06:12 +0600

    practice

--- Striver's A2Z Sheet/Step 2  Learn Important Sorting Techniques/Lec 1 Sorting-I/test.cpp
+++ Striver's A2Z Sheet/Step 2  Learn Important Sorting Techniques/Lec 1 Sorting-I/test.cpp
@@ -1,16 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define nl '\n'
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int t;
-    cin>>t;
-    int n = t*2;
-    int [n];
-    for(int i = 0;i<n;i==){
-        
-    }
-    return 0;
+int main(){
+    
 }
