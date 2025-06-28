abaa38622eacf247929e8c3edc2d54437242b76c
Mishad <mishadcs2002@gmail.com>
2025-06-28 20:14:21 +0600

    practice

---
+++ ZPractice/bs_coding_test.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n,m,k;
+    cin>>n>>m>>k;
+    int count = 0;
+    vector<int>a(n);
+    vector<int>b(n);
+    for(int i = 0;i<n;i++){
+    	cin>>a[i];
+    }
+    sort(a.begin(),a.end());
+    for(int i = 0;i<m;i++){
+    	cin>>b[i];
+    }
+    for(int i = 0;i<m;i++){
+    	for(int j = 0;j<n;j++){
+    		int check1 = a[j]+k;
+    		int check2 = a[j]-k;
+    		if(b[i]<check1 && b[i]>=check2){
+    			count++;
+    			break;
+    		}
+    	}
+    }
+    cout<<count;
+}
