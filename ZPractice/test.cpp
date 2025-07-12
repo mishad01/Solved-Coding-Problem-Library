eca37a96e80475722a4defe1a6cf8dc0da7d6b05
Mishad <mishadcs2002@gmail.com>
2025-07-13 00:39:14 +0600

    practice

--- ZPractice/test.cpp
+++ ZPractice/test.cpp
@@ -1,26 +1,13 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define nl '\n'
-int numIdenticalPairs(vector<int>& nums) {
-    int n = nums.size();
-    int count=0;
-    for(int i = 0;i<n;i++){
-        for(int j = 0;j<n;j++){
-            if(i<j && nums[i]==nums[j]){
-                count--;
-            }
-        }
-    }
-    return count;
-}
-int main() {
-    int n;
-    cin >> n;
-    vector<int>v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-        //cout << v[i] << nl;
-    }
-    cout<<numIdenticalPairs(v);
+class Person { 
+String name; 
+int age; 
+// Constructor 
+Person(this.name, this.age); 
+void sayHello() { 
+print("Hello, my name is $name and I am $age years old."); 
+} 
+} 
+void main() { 
+Person p2 = Person("Sakif", 23); 
+p2.sayHello();  // Output: Hello, my name is Sakif and I am 23 years old. 
 }
