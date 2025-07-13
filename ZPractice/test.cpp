50d9c1c1951d9f0bd4227a3d68aaf72582d4eea8
Mishad <mishadcs2002@gmail.com>
2025-07-13 09:28:15 +0600

    code arrange

--- ZPractice/test.cpp
+++ ZPractice/test.cpp
@@ -1,13 +1,7 @@
-class Person { 
-String name; 
-int age; 
-// Constructor 
-Person(this.name, this.age); 
-void sayHello() { 
-print("Hello, my name is $name and I am $age years old."); 
-} 
-} 
-void main() { 
-Person p2 = Person("Sakif", 23); 
-p2.sayHello();  // Output: Hello, my name is Sakif and I am 23 years old. 
+class Person {
+    String name = '';
+    int age = 0;
+    void sayHello() {
+        print("Hello, my name is $name and I am $age years old.");
+    }
 }
